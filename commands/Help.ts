eb596f03b47ac5c214d98e61a5d61e570ab1b99e
tmitim <igetemail@gmail.com>
2017-01-06 14:20:10 -0800

    Add: help command

---
+++ commands/Help.ts
@@ -0,0 +1,66 @@
+import { BotListener } from "./BotListener";
+import async = require('async');
+
+export class Help implements BotListener {
+  name = "help";
+  desc = "Shows this help menu";
+  hidden = true;
+  channels;
+  controller;
+  private commands : BotListener[];
+  constructor( controller ){
+    this.channels = ['direct_message','direct_mention','mention'];
+    this.controller = controller;
+  }
+
+  start() {
+    var help = this;
+    help.controller.hears('help', help.channels, function(bot,message) {
+      var commandString = "```\n";
+      help.commands
+      .filter(function(cmd) {
+        return (!cmd.hidden || message.text.indexOf("-h") != -1);
+      })
+      .sort(function (a, b) {
+        var commandA = a.name.toUpperCase();
+        var commandB = b.name.toUpperCase();
+        if (commandA < commandB) {
+          return -1;
+        }
+        if (commandA > commandB) {
+          return 1;
+        }
+        return 0;
+      })
+      .forEach(function(cmd) {
+        commandString += help.padColumn(cmd.name, 18) + cmd.desc + "\n";
+      })
+
+      commandString += "```";
+
+      async.series([
+        function(callback) {
+          bot.reply(message, "Here are my available commands - ");
+          callback(null, 1);
+        },
+        function(callback) {
+          setTimeout(function() {
+            bot.reply(message, commandString);
+            callback(null, 2);
+          }, 100);
+        }
+      ], function(err, results) {
+        if (err) console.log(err);
+      });
+    });
+  }
+
+  setAvailableCommands(commands : BotListener[]) {
+    this.commands = commands;
+  }
+
+  private padColumn(cmd, length) {
+    cmd += "                ";
+    return cmd.substring(0, length);
+  }
+}
--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ dotenv.config();
 import async = require('async');
 import { BotListener } from "./commands/BotListener";
 import { Analysis } from "./commands/Analysis";
+import { Help } from "./commands/Help";
 
 var controller = Botkit.slackbot({
   debug: false,
@@ -70,41 +71,13 @@ controller.on('rtm_close', function(bot, err) {
 
 start_rtm(spawnBot);
 
-controller.hears('help', ['direct_message','direct_mention','mention'], function(bot,message) {
-
-  var commandString = "```\n";
-  commands.forEach(function(command) {
-    if (!command.hidden || message.text.indexOf("-h") != -1) {
-      commandString += padColumn(command.name, 18) + command.desc + "\n"
-    }
-  })
-
-  commandString += "```";
-
-  async.series([
-    function(callback) {
-      bot.reply(message, "Here are my available commands - ");
-      callback(null, 1);
-    },
-    function(callback) {
-      setTimeout(function() {
-        bot.reply(message, commandString);
-        callback(null, 2);
-      }, 100);
-    }
-  ], function(err, results) {
-    if (err) console.log(err);
-  });
-});
-
-function padColumn(cmd, length) {
-  cmd += "                ";
-  return cmd.substring(0, length);
-}
-
-var commands = [];
+var commands : BotListener[] = [];
 commands.push(analysis);
+var help = new Help(controller);
+help.setAvailableCommands(commands);
+commands.push(help);
 
 commands.forEach(function(command) {
+  console.log(command.name);
   command.start();
 })
